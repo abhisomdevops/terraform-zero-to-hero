ef98f9be0fec48cfd3af71cbdc93b49c545c961d
abhisomdevops <somabhi2204@gmail.com>
2026-07-16 23:52:47 +0000

    added comment to outputs.tf

--- Day-3/outputs.tf
+++ Day-3/outputs.tf
@@ -1,3 +1,4 @@
+# ec2 public ip output
 output "public-ip-address" {
-  value = module.ec2_instance.public-ip-address
+  value = module.ec2_instance.public-ip-address # reference instance public_ip fron the module
 }
