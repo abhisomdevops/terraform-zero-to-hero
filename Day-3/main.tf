17edf93a1009003e9e67a002493b4dbab2084195
abhisomdevops <somabhi2204@gmail.com>
2026-07-16 23:16:53 +0000

    Day-3 changes, add .gitignore

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0b6d9d3d33ba97d99" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-5192647a" # replace this
 }
